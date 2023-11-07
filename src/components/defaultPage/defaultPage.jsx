0e414427f44434a6d6302898e3162193fd98e9e3
moimenwagdy <moimenwy@gmail.com>
2023-11-07 03:54:47 +0200

    image path reset

--- src/components/defaultPage/defaultPage.jsx
+++ src/components/defaultPage/defaultPage.jsx
@@ -2,7 +2,7 @@ export default function DefaultPage({ clickHandler }) {
   return (
     <div className="flex flex-col justify-center items-center text-center w-3/4 space-y-6">
       <img
-        src="/src/assets/no-projects.png"
+        src="./src/assets/no-projects.png"
         alt="No image"
         className="w-1/12"></img>
       <h4 className="font-bold text-2xl text-black/60">No Project Selected</h4>
